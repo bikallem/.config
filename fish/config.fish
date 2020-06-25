d9e92aab6a7bfb42e0245f98ae63b7f40c97b196
Nathan Smith <nathansmith@stripe.com>
2020-06-25 14:11:03 -0700

    Fix some PATH stuff

--- fish/config.fish
+++ fish/config.fish
@@ -5,6 +5,7 @@ if status --is-login; and status --is-interactive
   alias stripe='cd ~/stripe'
   alias pay-server='cd ~/stripe/pay-server'
   alias uppsala='cd ~/stripe/uppsala'
+  alias ls='exa'
 
   # PATH settings
   # GNU utils
@@ -13,10 +14,4 @@ if status --is-login; and status --is-interactive
   set -x PATH /usr/local/opt/curl/bin $PATH
   # Use installed LLVM tools
   set -x PATH /usr/local/opt/llvm/bin $PATH
-  # Python
-  set -x PATH "/usr/local/opt/python@3.8/bin" $PATH
-  # Ruby
-  set -x PATH /usr/local/lib/ruby/gems/2.7.0/bin /usr/local/opt/ruby/bin $PATH
-  # Rust
-  set -x PATH $HOME/.cargo/bin $PATH
 end
