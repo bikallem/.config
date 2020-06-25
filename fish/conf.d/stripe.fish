6af4bf3c5dea81b645c91c568de391305f4042e7
Nathan Smith <nathansmith@stripe.com>
2020-06-25 09:00:35 -0700

    Update fish config with Stripe stuff

---
+++ fish/conf.d/stripe.fish
@@ -0,0 +1,13 @@
+if status --is-login; and status --is-interactive; and test -e $HOME/.stripeproxy
+    # Add the stripe tools paths
+    set -x PATH $HOME/stripe/space-commander/bin $PATH
+    set -x PATH $HOME/stripe/password-vault/bin $PATH
+    set -x PATH $HOME/stripe/henson/bin $PATH
+    set -x PATH $HOME/.rbenvs/shims $PATH
+    set -x PATH $HOME/.rbenvs/bin $PATH
+
+    # Activate the space-commander env
+    source (rbenv init -|psub)
+    source (nodenv init -|psub)
+    source $HOME/stripe/space-commander/bin/sc-env-activate.fish
+end
--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,11 @@
 if status --is-login; and status --is-interactive
+  # Aliases
+  # TODO: is there a better place to put these?
+  alias config='cd ~/.config'
+  alias stripe='cd ~/stripe'
+  alias pay-server='cd ~/stripe/pay-server'
+  alias uppsala='cd ~/stripe/uppsala'
+
   # PATH settings
   # GNU utils
   set -x PATH /usr/local/opt/{coreutils,findutils,grep,gnu-sed,gnu-time,make,gnu-units}/libexec/gnubin $PATH
