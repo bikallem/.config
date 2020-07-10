834734fcc6dc3dec05c80cae866688085eb3c43f
Nathan Smith <nathansmith@stripe.com>
2020-07-10 09:13:26 -0700

    Add Spotify URLs to open in app

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -170,9 +170,11 @@ k:bind({}, 'escape', function() k:exit() end)
 hs.loadSpoon("URLDispatcher")
 local Zoom = "us.zoom.xos"
 local Chrome = "com.google.Chrome"
+local Spotify = "com.spotify.client"
 spoon.URLDispatcher.url_patterns = {
   {"https?://zoom.us/j/", Zoom},
-  {"https?://%w+.zoom.us/j/", Zoom}
+  {"https?://%w+.zoom.us/j/", Zoom},
+  {"https?://open.spotify.com/", Spotify},
 }
 spoon.URLDispatcher.default_handler = Chrome
 spoon.URLDispatcher:start()
