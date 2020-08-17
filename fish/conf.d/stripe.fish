c50e0eb07788e7773846d5e0f41800782f60c841
Nathan Smith <nathansmith@stripe.com>
2020-08-17 10:26:48 -0700

    Fish cleanup

--- fish/conf.d/stripe.fish
+++ fish/conf.d/stripe.fish
@@ -1,13 +1,18 @@
 if status --is-login; and status --is-interactive; and test -e $HOME/.stripeproxy
-    # Add the stripe tools paths
-    set -x PATH $HOME/stripe/space-commander/bin $PATH
-    set -x PATH $HOME/stripe/password-vault/bin $PATH
-    set -x PATH $HOME/stripe/henson/bin $PATH
-    set -x PATH $HOME/.rbenvs/shims $PATH
-    set -x PATH $HOME/.rbenvs/bin $PATH
+  # Aliases
+  alias stripe='cd ~/stripe'
+  alias pay-server='cd ~/stripe/pay-server'
+  alias uppsala='cd ~/stripe/uppsala'
 
-    # Activate the space-commander env
-    source (rbenv init -|psub)
-    source (nodenv init -|psub)
-    source $HOME/stripe/space-commander/bin/sc-env-activate.fish
+  # Add the stripe tools paths
+  set -x PATH $HOME/stripe/space-commander/bin $PATH
+  set -x PATH $HOME/stripe/password-vault/bin $PATH
+  set -x PATH $HOME/stripe/henson/bin $PATH
+  set -x PATH $HOME/.rbenvs/shims $PATH
+  set -x PATH $HOME/.rbenvs/bin $PATH
+
+  # Activate the space-commander env
+  source (rbenv init -|psub)
+  source (nodenv init -|psub)
+  source $HOME/stripe/space-commander/bin/sc-env-activate.fish
 end
--- fish/config.fish
+++ fish/config.fish
@@ -1,12 +1,4 @@
 if status --is-login; and status --is-interactive
-  # Aliases
-  # TODO: is there a better place to put these?
-  alias config='cd ~/.config'
-  alias stripe='cd ~/stripe'
-  alias pay-server='cd ~/stripe/pay-server'
-  alias uppsala='cd ~/stripe/uppsala'
-  alias ls='exa'
-
   # PATH settings
   # GNU utils
   set -x PATH /usr/local/opt/{coreutils,findutils,grep,gnu-sed,gnu-time,make,gnu-units}/libexec/gnubin $PATH
