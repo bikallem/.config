47d097fa27a14545a965eac61457c8a8875c0ce9
Nathan Smith <nathansmith@stripe.com>
2020-09-08 14:27:29 -0700

    wip

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,10 +1,22 @@
 hs.loadSpoon("ReloadConfiguration")
 spoon.ReloadConfiguration:start()
 
--- Variable names, no they aren't garbage collected
+-- Variable names, so they aren't garbage collected
 b = require('bluetooth')
 c = require('caffeine')
 p = require('paste')
 u = require('urls')
 f = require('focus')
 w = require('window-management')
+
+-- VimMode
+local VimMode = hs.loadSpoon('VimMode')
+local vim = VimMode:new()
+
+vim
+  :disableForApp('Code')
+  :disableForApp('iTerm')
+  :disableForApp('iTerm 2')
+  :disableForApp('zoom.us')
+  :enterWithSequence('jk')
+  :shouldDimScreenInNormalMode(false)
